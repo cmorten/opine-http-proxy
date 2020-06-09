14b9d906e2b4d6ae07d8ade21da28957c43b8e0e
asos-craigmorten <craig.morten@asos.com>
2020-06-09 22:37:47 +0100

    fix: version

--- version.ts
+++ version.ts
@@ -1,9 +1,15 @@
 /** 
- * Version of Opine.
+ * Version of opine-http-proxy.
  */
 export const VERSION: string = "1.0.0";
 
 /**
  * Supported versions of Deno.
  */
-export const DENO_SUPPORTED_VERSIONS: string[] = ["1.0.0", "1.0.2", "1.0.3"];
+export const DENO_SUPPORTED_VERSIONS: string[] = [
+  "1.0.0",
+  "1.0.2",
+  "1.0.3",
+  "1.0.4",
+  "1.0.5",
+];
