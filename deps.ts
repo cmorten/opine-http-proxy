ad64944c6ebdb05b78ea599e9520db64ab869f59
cmorten <craig.morten@hotmail.co.uk>
2021-04-26 17:31:41 +0100

    feat: deno 1.9.2

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { join } from "https://deno.land/std@0.85.0/path/mod.ts";
-export { read } from "https://deno.land/x/opine@1.1.0/src/middleware/bodyParser/read.ts";
-export { parseUrl } from "https://deno.land/x/opine@1.1.0/src/utils/parseUrl.ts";
+export { join } from "https://deno.land/std@0.95.0/path/mod.ts";
+export { read } from "https://deno.land/x/opine@1.3.3/src/middleware/bodyParser/read.ts";
+export { parseUrl } from "https://deno.land/x/opine@1.3.3/src/utils/parseUrl.ts";
